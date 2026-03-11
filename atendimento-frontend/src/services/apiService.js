5e34c6e44d9954ea163c7de7a9bf52a56d2cd59a
MateusCortez <mateusacortez2@hotmail.com>
2026-03-11 05:45:27 -0300

    Alterando chamadas da URL da API 3

--- atendimento-frontend/src/services/apiService.js
+++ atendimento-frontend/src/services/apiService.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
+export const API_URL = "https://atendimentopds.onrender.com/api";
+
 export const getHello = async () => {
-  const response = await axios.get(`${BASE_URL}/hello`);
+  const response = await axios.get(`${API_URL}/hello`);
   return response.data;
 };
 
 // endereço do backend
-export const API_URL = "https://atendimentopds.onrender.com/api";
